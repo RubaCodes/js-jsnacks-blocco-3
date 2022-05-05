6fa9fae1b8275e0aa3570b2d8f83785cd86e1bf3
rubacodes <rubacodes@gmail.com>
2022-05-05 14:24:36 +0200

    input utente numero e validazione

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,2 +1,8 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
+let N = Number(prompt('Inserisci un numero'));
+
+while(isNaN(N)){
+    N = Number(prompt('Inserisci un numero, ho detto Numero'));
+}
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
