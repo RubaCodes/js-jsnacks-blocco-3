c37902ca2f040630516a48120cc6de9f2ad027d6
rubacodes <rubacodes@gmail.com>
2022-05-05 15:17:41 +0200

    refactoring

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,30 +1,26 @@
 // Partendo da un array di numeri, 
 const array = [];
-for (let i = 0; i < 20; i++){
-    array.push(Math.ceil(Math.random() *100));
+for (let i = 0; i < 20; i++) {
+    array.push(Math.ceil(Math.random() * 100));
 }
-
-
-// Crea due tag div con due id diversi: //
+// Crea due tag div con due id diversi: 
 const body = document.querySelector('body');
 //creazione div
 const primoDiv = document.createElement('div');
 const secondoDiv = document.createElement('div');
 //assegnazione id
-primoDiv.id = 'primo-div;'
-secondoDiv.id = 'secondo-div;'
+primoDiv.id = 'dispari';
+secondoDiv.id = 'pari';
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 primoDiv.style.color = 'red';
 secondoDiv.style.color = 'green';
-
-for(let i = 0; i< array.length; i++){
-    if(array[i] % 2 !== 0){
+//Stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
         primoDiv.append(`${array[i]} , `)
-    }else{
+    } else {
         secondoDiv.append(`${array[i]} , `);
     }
 }
-
-//Stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
-    //appeso div al body
- body.append(primoDiv, secondoDiv);
+//appeso i div al body
+body.append(primoDiv, secondoDiv);
