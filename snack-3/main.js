2ce9d9b8e0be20c090941f1ae0b47dec9ec54057
rubacodes <rubacodes@gmail.com>
2022-05-05 15:02:03 +0200

    colori dei div e commenti

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,10 +2,14 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 const body = document.querySelector('body');
+//creazione div
 const primoDiv = document.createElement('div');
 const secondoDiv = document.createElement('div');
-
+//assegnazione id
 primoDiv.id = 'primo-div;'
 secondoDiv.id = 'secondo-div;'
-
-body.append(primoDiv, secondoDiv)
+//colore testto
+primoDiv.style.color = 'red';
+secondoDiv.style.color = 'green';
+//appeso div al body
+body.append(primoDiv, secondoDiv);
