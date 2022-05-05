40bc89386e2570892c8ae2e047043acf0086e5d1
rubacodes <rubacodes@gmail.com>
2022-05-05 15:05:51 +0200

    aggiunto generatore random di numeri

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,6 +1,4 @@
 // Crea due tag div con due id diversi: //
-// un div avrà il testo colorato di rosso mentre l’altro di verde.
-// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 const body = document.querySelector('body');
 //creazione div
 const primoDiv = document.createElement('div');
@@ -8,8 +6,14 @@ const secondoDiv = document.createElement('div');
 //assegnazione id
 primoDiv.id = 'primo-div;'
 secondoDiv.id = 'secondo-div;'
-//colore testto
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
 primoDiv.style.color = 'red';
 secondoDiv.style.color = 'green';
-//appeso div al body
-body.append(primoDiv, secondoDiv);
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const array = [];
+for (let i = 0; i < 20; i++){
+    array.push(Math.ceil(Math.random() *100));
+}
+
+    //appeso div al body
+    body.append(primoDiv, secondoDiv);
