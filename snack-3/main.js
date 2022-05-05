9b7392e109f092e0f76c7138747ea8e672206fa4
rubacodes <rubacodes@gmail.com>
2022-05-05 14:59:53 +0200

    creato due div con id diversi

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,3 +1,11 @@
 // Crea due tag div con due id diversi: //
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const body = document.querySelector('body');
+const primoDiv = document.createElement('div');
+const secondoDiv = document.createElement('div');
+
+primoDiv.id = 'primo-div;'
+secondoDiv.id = 'secondo-div;'
+
+body.append(primoDiv, secondoDiv)
