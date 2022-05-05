d5a820423e1591827f3a52dbeff68072153d0bd7
rubacodes <rubacodes@gmail.com>
2022-05-05 13:12:36 +0200

    fix alpha

--- snack-1/main.js
+++ snack-1/main.js
@@ -2,19 +2,20 @@
 let array = [];
 let sum = 0;
 
-while(sum < 49){
+while(sum < 50){
     let temp = Number(prompt("inserisci un numero"));
     while(isNaN(temp)){
         temp = Number(prompt("inserisci un numero, ho detto NUMERO"));
+        
     }
-    if(sum + temp < 50){
+    sum +=temp;
+    if(sum < 50){
         array.push(temp);
-        sum +=temp;
-    console.log(array,sum);
-    }else{
-        console.log(`somma maggiore di 50,inserire numero minore di ${50 - sum}`);
     }
+    console.log(array,sum);
 }
+
+
 
 
 //da reimplememntare con do while
